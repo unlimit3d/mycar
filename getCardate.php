708534fd4147828a8f263a6ca5f8a7d85d66c826
unlimit3d <i.unlimit3d@gmail.com>
2022-09-27 09:59:57 +0700

    test db

--- getCardate.php
+++ getCardate.php
@@ -1,10 +1,13 @@
 <?php
-require_once('function.php');
+require_once 'db.php';
+$conn = CON_CARDATA();
+exit();
+$sql = "SELECT * FROM tb_cars";
+// mysql_query($conn, )
+
 FIX_PHP_CORSS_ORIGIN();
 $datacar = array();
 
-$conn = CON_CARDATA();
-$sql = "SELECT * FROM test.cars";
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 //   }
@@ -12,16 +15,15 @@ $sql = "SELECT * FROM test.cars";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    // echo "name: " . $row["name"]. " - model: " . $row["model"]. " price" . $row["price"]. "<br>";
-    $datacar[] = $row;
-  }
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        // echo "name: " . $row["name"]. " - model: " . $row["model"]. " price" . $row["price"]. "<br>";
+        $datacar[] = $row;
+    }
 //   for ($datacar = array (); $row = $result->fetch_assoc(); $datacar[] = $row);
-//   print_r($datacar);
-   echo json_encode($datacar);
+    //   print_r($datacar);
+    echo json_encode($datacar);
 } else {
-   echo "{'results':0'}";
+    echo "{'results':0'}";
 }
 CloseCON_CARDATA($conn);
-?>
