f4d0f6c80597765bb81d1b2029f0ad3b6672afb7
unlimit3d <i.unlimit3d@gmail.com>
2022-09-27 10:37:31 +0700

    select cars

--- db.php
+++ db.php
@@ -3,11 +3,11 @@ function CON_CARDATA()
 {
     $dbhost = "127.0.0.1"; // localhost
     $dbuser = "root";
-    $dbpass = "1";
+    $dbpass = "";
     $db = "mycar_db";
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . mysqli_error());
     // $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
-    echo 'db connected';
+    // echo 'db connected';
     return $conn;
 }
 
--- getCardate.php
+++ getCardate.php
@@ -1,8 +1,26 @@
 <?php
+error_reporting(0);
 require_once 'db.php';
-$conn = CON_CARDATA();
+
+try {
+    $conn = CON_CARDATA();
+    //code...
+    $sql = "SELECT * FROM tb_cars";
+
+    $result = mysqli_query($conn, $sql);
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    // echo json_encode($data);
+    echo json_encode(array('error' => '', 'data' => $data));
+} catch (Exception $e) {
+    //throw $th;
+    echo json_encode(array('error' => $e->getMessage . 'Error select ข้อมูลไม่ได้'));
+}
+
 exit();
-$sql = "SELECT * FROM tb_cars";
+
 // mysql_query($conn, )
 
 FIX_PHP_CORSS_ORIGIN();
