00a31f1724d7072e885e8ea49a61ff7aa8fa63a2
unlimit3d <i.unlimit3d@gmail.com>
2022-09-28 14:18:24 +0700

    add jwt

--- getCardate.php
+++ getCardate.php
@@ -1,11 +1,22 @@
 <?php
-error_reporting(0);
-// require_once 'function.php';
+// error_reporting(0);
+
+require 'vendor/autoload.php';
 require_once 'db.php';
+
 FIX_PHP_CORSS_ORIGIN();
+
+use \Firebase\JWT\JWT;
+
 try {
+
+    // $privateKey = SECRET_KEY;
+    // echo $jwt = getBearerToken();
+
+    // $decoded = JWT::decode($jwt, $privateKey, ['HS256']);
+    // print_r($decoded);
     $conn = CON_CARDATA();
-    //code...
+//code...
     $sql = "SELECT * FROM tb_cars";
 
     $result = mysqli_query($conn, $sql);
@@ -13,7 +24,7 @@ try {
     while ($row = mysqli_fetch_assoc($result)) {
         $data[] = $row;
     }
-    // echo json_encode($data);
+// echo json_encode($data);
     echo json_encode(array('error' => '', 'data' => $data));
 } catch (Exception $e) {
     //throw $th;
